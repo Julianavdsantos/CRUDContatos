017795a4f5772b5c1806eb38ee9907c1cab580b9
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2024-05-11 13:39:17 -0300

    Atualizar o db.js

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"123@456",
+    password:"3333",
     database: "crud2"
 })
 
